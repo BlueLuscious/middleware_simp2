041506c4359656650f8b78c636116c3fa108d623
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-12-13 18:54:53 -0300

    update kernel.php
    - added Simp2Middleware

--- app/app/Http/Kernel.php
+++ app/app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Simp2Middleware::class,
         ],
     ];
 
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'simp2' => \App\Http\Middleware\Simp2Middleware::class,
     ];
 }
